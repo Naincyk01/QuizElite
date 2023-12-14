6b38960f2e23bb8766343812713846ecfa5a31ac
Naincy <naincyk15042004@gmail.com>
2023-12-14 22:49:08 +0530

    Made the login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
+import { Outlet,RouterProvider,createBrowserRouter } from 'react-router-dom';
+import Login from "../src/components/Login"
 import './App.css';
-function App() {
+const AppLayout = () => {
   return (
-    <div className="bg-red-300">
-
+    <div className="bg-black h-screen w-full">
+      <Outlet/>
     </div>
   );
 }
-
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    children:[
+      {
+        path:"/",
+        element:<Login/>
+      },
+    ]
+  }
+])
+const App = () => {
+  return (
+      <RouterProvider router ={appRouter}/>
+  )
+}
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,37 @@
+import { FaLongArrowAltRight } from "react-icons/fa";
+import login from "../assests/login.png";
+
+const Login = () => {
+  return (
+    <div className="h-screen flex justify-center items-center bg-slate-200">
+      <div className="flex w-[70%] h-[80%] border-2 bg-white">
+
+        <div className="flex justify-center items-center w-2/3">
+          <div className="flex flex-col w-[400px] h-[500px] gap-y-10 items-center justify-center">
+            <div className="text-purple-900 font-bold text-xl">
+              User Login
+            </div>
+            <div className="flex flex-col gap-y-8">
+              <input className="text-purple-900 px-2 rounded-2xl h-[37px] w-[250px] border-2 font-semibold" placeholder="Email"/>
+              <input className="px-2 text-purple-800 rounded-2xl h-[37px] w-[250px] font-medium border-2" placeholder="password"/>
+            </div>
+            <button className="text-purple-900 border-2 px-10 rounded-3xl h-10 flex justify-center items-center w-48 bg-purple-400">Login</button>
+            <button className="text-purple-900 flex justify-center items-center gap-x-2">
+              Create account
+              <FaLongArrowAltRight className="mt-1"/>
+              </button>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center w-1/3 bg-purple-500">
+          <div className="">
+            <img src={login} alt="login" />
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Login;
