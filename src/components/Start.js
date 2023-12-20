c479b4d55274785c5c50c6f10a0acc9e9d6801cd
Naincy <naincyk15042004@gmail.com>
2023-12-20 22:13:07 +0530

    Start of quiz

--- src/components/Start.js
+++ src/components/Start.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Start = () => {
   return (
-    <div>Start</div>
+    <div className='bg-green-100 flex h-screen w-full justify-center items-center font-extrabold text-6xl text-green-500'>WELCOME</div>
   )
 }
 
