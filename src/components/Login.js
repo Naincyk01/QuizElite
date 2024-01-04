115a4cf905948968e832942005e324061fcda7f8
Naincy <naincyk15042004@gmail.com>
2024-01-04 23:19:39 +0530

    Some changes to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,36 +34,36 @@ const Login = () => {
   }, [name, password]);
 
   return (
-    <div className="h-screen flex justify-center items-center bg-purple-100">
+    <div className="h-screen w-full flex justify-center items-center bg-purple-100">
       <div
         className="flex w-[70%] h-[80%] border-2 bg-white max-lg:w-full max-lg:mx-12 max-md:flex-col 
-      max-md:items-center max-md:justify-center max-sm:mx-4  max-md:h-auto max-md:py-12"
+      max-md:items-center max-md:justify-center max-sm:mx-4 max-md:h-auto max-md:py-12 border-purple-200 max-md:gap-y-6"
       >
         
-        <div className="flex justify-center items-center w-1/3 bg-purple-500  max-md:w-full">
-          <div className="max-sm:w-60 max-sm:h-60">
+        <div className="flex justify-center items-center w-[50%] max-md:w-full">
+          <div className="bg-purple-500 rounded-full w-80 h-80">
             <img src={loginImg} alt="login" />
           </div>
         </div>
 
-        <div className="flex justify-center items-center w-2/3">
+        <div className="flex justify-center items-center w-1/2 max-md:w-full">
           <div className="flex flex-col w-[400px] h-[500px] gap-y-10 items-center justify-center">
-            <div className="text-purple-900 font-bold text-xl">User Login</div>
-            <div className="flex flex-col gap-y-8">
+            <div className="text-purple-900 text-2xl font-extrabold">User Login</div>
+            <div className="flex flex-col gap-y-6">
               <input
-                className="text-purple-900 px-2 rounded-2xl h-[37px] w-[250px] border-2 font-semibold"
+                className="text-purple-900 px-2 rounded-full h-[46px] w-[250px] bg-gray-100 font-semibold"
                 placeholder="Email/Username"
                 onChange={(e) => setName(e.target.value)}
               />
               <input
-                className="px-2 text-purple-800 rounded-2xl h-[37px] w-[250px] font-medium border-2"
-                placeholder="password"
+                className="px-2 text-purple-800 rounded-full h-[46px] w-[250px] bg-gray-100 font-medium"
+                placeholder="Password"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <Link to={isAuthenticated}>
               <button
-                className="text-purple-900 border-2 px-10 rounded-3xl h-10 flex justify-center items-center w-48 bg-purple-400"
+                className="text-purple-900 px-10 rounded-3xl h-12 flex justify-center items-center bg-purple-400 w-52 font-semibold text-xl"
                 onClick={() => handleVisibility()}
               >
                 Login
@@ -73,7 +73,7 @@ const Login = () => {
             <div className={`${visibility} text-red-500`}>
               username of password is incorrect
             </div>
-            <button className="text-purple-900 flex justify-center items-center gap-x-2">
+            <button className="text-purple-900 flex justify-center items-center gap-x-2 text-lg">
               <Link to="./signIn">Create account</Link>
               <FaLongArrowAltRight className="mt-1" />
             </button>
