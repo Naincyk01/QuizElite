e97de7ede1d238fd448b13bb687bbc993513791c
Naincy <naincyk15042004@gmail.com>
2024-01-07 22:44:52 +0530

    Fixed some css of start page and made it responsive

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Start = () => {
-
-  const currentusername = useSelector(store => store.signData.currentUser);
+  const currentusername = useSelector((store) => store.signData.currentUser);
   return (
-     <div className='bg-green-100 flex h-screen w-full justify-center items-center flex-col'>
-     <div className='flex justify-center flex-col items-center font-extrabold'>
-      <div className='text-6xl flex max-sm:text-5xl'>WELCOME</div>
-      <div className='text-4xl capitalize tracking-wider'>{currentusername.name}</div>
-     </div>
-     <Link to="/ques/ques1">Start the Quiz</Link>
-      </div>
-  )
-}
+    <div className="bg-purple-100 flex h-screen w-full justify-center items-center">
+      <div className="flex h-[80%] w-[70%] justify-center items-center flex-col bg-white border-2 border-purple-200 max-lg:w-full max-lg:mx-12">
+       
+        
+          <div className="text-6xl flex max-sm:text-[2.8rem] text-purple-900 font-bold">
+            WELCOME
+          </div>
+          <div className="text-4xl capitalize tracking-wider text-purple-900 font-bold">
+            {currentusername.name}
+          </div>
+          <Link to="/ques/ques1" className="text-purple-900 font-bold text-3xl">
+            Start the Quiz💡
+          </Link>
 
-export default Start
+        </div>
+    </div>
+  );
+};
+
+export default Start;
