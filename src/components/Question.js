7eee36559d2c57bffc04446d7dd01a0985abf957
Naincy <naincyk15042004@gmail.com>
2024-01-09 23:42:52 +0530

    Made question pages responsive

--- src/components/Question.js
+++ src/components/Question.js
@@ -11,37 +11,40 @@ const Question = ({question,opt1,opt2,opt3,opt4,answer,link}) => {
     }
   };
   return (
-    <div className="text-white flex flex-col justify-center items-center h-full w-screen gap-y-6">
-      <div className="text-4xl">{"" + question}</div>
-      <div className="flex flex-col text-2xl">
+    <div className="text-white flex justify-center items-center h-screen w-full bg-purple-100">
+      <div className="flex flex-col justify-center items-center w-2/3  h-[80%] gap-y-14">
+      <div className="text-3xl text-purple-800 font-semibold w-full h-32 flex justify-center items-center">{"" + question}</div>
+      <div className="flex flex-col text-xl">
         <Link
           to={link}
-          className="border-2 border-purple-800 px-2 m-2 bg-purple-500 rounded-md h-10"
+          className="border-2 border-purple-800 px-2 m-2 bg-purple-500 rounded-md h-12 w-[190px]"
           onClick={() => handleclick(opt1)}
         >
           {"" + opt1}
         </Link>
         <Link
           to={link}
-          className="border-2 border-purple-800 px-2 m-2 bg-purple-500 rounded-md h-10"
+          className="border-2 border-purple-800 px-2 m-2 bg-purple-500 rounded-md h-12 w-[190px]"
           onClick={() => handleclick(opt2)}
         >
          {"" + opt2}
         </Link>
         <Link
           to={link}
-          className="border-2 border-purple-800 px-2 m-2 bg-purple-500 rounded-md h-10"
+          className="border-2 border-purple-800 px-2 m-2 bg-purple-500 rounded-md h-12 w-[190px]"
           onClick={() => handleclick(opt3)}
         >
          {"" + opt3}
         </Link>
         <Link
           to={link}
-          className="border-2 border-purple-800 px-2 m-2 bg-purple-500 rounded-md h-10"
+          className="border-2 border-purple-800 px-2 m-2 bg-purple-500 rounded-md h-12 w-[190px]"
           onClick={() => handleclick(opt4)}
         >
           {"" + opt4}
         </Link>
+      </div>
+      
       </div>
     </div>
   );
