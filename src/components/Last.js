ba1dbf961d1c75224e2ab5c535572d65dda696fc
Naincy <naincyk15042004@gmail.com>
2024-01-13 23:22:40 +0530

    Made score page responsive

--- src/components/Last.js
+++ src/components/Last.js
@@ -12,15 +12,15 @@ const Last = () => {
   }
   return (
     <div className="text-purple-500 h-screen flex flex-col items-center justify-center gap-y-8 bg-purple-100">
-      <div className="w-80 h-80 rounded-full border-4 border-purple-600 border-dashed flex items-center justify-center ">
+      <div className="w-80 h-80 rounded-full border-4 border-purple-600 border-dashed flex items-center justify-center max-sm:w-64 max-sm:h-64">
         <div
           className="w-72 h-72 rounded-full border-8 border-purple-500 animate-pulse flex items-center justify-center font-bold
-                text-9xl"
+                text-9xl max-sm:w-60 max-sm:h-60 max-sm:text-7xl"
         >
           {"" + score}/5
         </div>
       </div>
-      <div className="font-bold text-4xl">Your Score.........</div>
+      <div className="font-bold text-4xl max-sm:text-2xl">Your Score.........</div>
     </div>
   );
 };
